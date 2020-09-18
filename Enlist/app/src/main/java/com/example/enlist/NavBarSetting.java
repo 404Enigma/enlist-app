9b3110298943a97caa45bece7400ef5158aa5d39
prakhar-agarwall <prakharaga21@gmail.com>
2020-09-18 19:34:51 +0530

    UI changed

--- Enlist/app/src/main/java/com/example/enlist/NavBarSetting.java
+++ Enlist/app/src/main/java/com/example/enlist/NavBarSetting.java
@@ -1,13 +1,19 @@
 package com.example.enlist;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,6 +23,7 @@ public class NavBarSetting extends AppCompatActivity {
 
     ListView settings_listView;
     String[] items = {"Profile","Class Group","Log out"};
+    MyAdapter adapter;
 
     private GoogleSignInClient mGoogleSignInClient;
 
@@ -34,7 +41,7 @@ public class NavBarSetting extends AppCompatActivity {
 
         settings_listView = findViewById(R.id.settings_listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(NavBarSetting.this,android.R.layout.simple_list_item_1,items);
+        adapter = new MyAdapter(NavBarSetting.this, items);
         settings_listView.setAdapter(adapter);
 
         settings_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -59,5 +66,29 @@ public class NavBarSetting extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String mHeading[];
+
+        MyAdapter (Context c,String Heading[]){
+            super(c,R.layout.custom_list_view_settings,items);
+            this.context = c;
+            this.mHeading = Heading;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.custom_list_view_settings,parent,false);
+            TextView shop_name = row.findViewById(R.id.heading);
+
+            shop_name.setText(mHeading[position]);
+
+            return row;
+        }
     }
 }
